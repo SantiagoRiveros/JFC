34ce828f82137e8fa08eba05fc84d4ee198d47df
Fernando Leiva <efeleiva@gmail.com>
2021-06-23 14:10:02 -0300

    Css

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,33 +1,55 @@
-import style from "../../CSS/home.module.scss";
+import style from "../../CSS/home.css";
+import React from "react";
+import img1 from "../../img/1.jpg";
+import img2 from "../../img/2.jpg";
+import img3 from "../../img/3.jpg";
+import img4 from "../../img/4.jpg";
 
 export default function Home() {
   return (
-    <div>
-      <div className={style.fondo}>
-        <h1>ELIGE BIEN,</h1>
-        <h1>VIVE BIEN.</h1>
-        <h6>Solo estás a una decisión de distancia...</h6>
+    <div class="container">
+      <div class="fondo1">
+        <h1 class="img1h1">ELIGE BIEN,</h1>
+        <h1 class="img1h12">VIVE BIEN.</h1>
+        <h6 class="img1h6"> Solo estás a una decisión de distancia...</h6>
       </div>
-      <div>
-        <h3>EL MEJOR SERVICIO POST-VENGA</h3>
-        <h1>UNA EMPRESA</h1>
-        <h1>DE MULTISERVICIOS</h1>
-        <h4>
+      <img src={img1} class="fondo2"/>
+      <div class="fondo2txt">
+      <h6 class="img2h6">EL MEJOR SERVICIO POST-VENTA</h6>
+      <h1 class="img2h1">UNA EMPRESA</h1>
+      <div class="containerh1">
+        <h1 class="img2h1">DE </h1>
+        <h1 class="img2h12">MULTISERVICIOS</h1>
+        </div>
+        <div class="containerh6">
+        <p id="img2h62">
           Cuando se trata de ayudara conseguir el hogar ideal JFC Consultores es
-          tu mejor opción, con profesionales especializados en cada ambito
-          tenemos la posibilidad de brindarte un servicio completo de principio
+          tu <span>mejor opción:</span> con profesionales especializados en cada ambito
+          tenemos la posibilidad de brindarte un <span>servicio completo</span> de principio
           a fin.
-        </h4>
-        <h4>
+        </p>
+        <p id="img2h62">
           Contamos con proyectos de los mas variados para cubrir todas las
           necesidades de nuestros clientes: apartamentos, casas, proyectos de
-          playa y todo de primer nivel. No te lo vas a querer perder...
-        </h4>
-        <ul>
-          <li>Bienes Raices</li>
-          <li>Legales</li>
-          <li>Contratistas</li>
-        </ul>
+          playa y todo de primer nivel.
+        </p>
+        <p class="img2h63">No te lo vas a querer perder...</p>
+        </div>
+        <div style={{ marginTop: "4rem", marginLeft: "20rem", marginRight: "20rem", display: "flex", justifyContent: "space-around"
+}}>
+        <div class="containerli">
+        <img src={img2} class="imgli"/>
+        <span class="lii">Bienes Raices</span>
+          </div>
+          <div class="containerli">
+          <img src={img3} class="imgli"/>
+          <span class="lii">Legales</span>
+          </div>
+          <div class="containerli">
+          <img src={img4} class="imgli"/>
+          <span class="lii">Contratistas</span>
+          </div>
+        </div>
       </div>
     </div>
   );
