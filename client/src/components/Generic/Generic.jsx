d4825a951b4c4bd303e1ed834b24084236c69b65
Fernando Leiva <efeleiva@gmail.com>
2021-06-24 11:02:06 -0300

    generic

--- client/src/components/Generic/Generic.jsx
+++ client/src/components/Generic/Generic.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import style from "../../CSS/generic.css";
 
 export default function Generic() {
   return (
-    <div>
-      <h1>Generic Component</h1>
-      <h3>
+    <div class="container">
+      <div class="fondo">
+      <h1 class="title">Generic Component</h1>
+      <h3 class="text">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod
         tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea
         commodi consequat. Quis aute iure reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat
+        cillum dolore eu fugiat nulla pariatur. Excipteur sint obcaecat
         cupiditat non proident, sunt in culpa qui officia deserunt mollit anim
         id est laborum.
       </h3>
+      </div>
     </div>
   );
 }
